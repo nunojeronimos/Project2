fb216234f11180b2ca5cde9102490df0360b67e1
nunojeronimos <nuno,jeroimo@jeronimos.com.pt>
2023-07-26 20:50:09 +0100

    1

--- static/script.js
+++ static/script.js
@@ -37,7 +37,7 @@ function Register() {
   document.getElementById("register_popup").classList.add("active");
 }
 
-function savePicture() {
+async function savePicture() {
   var canvas = document.getElementById("canvas");
   var context = canvas.getContext("2d");
   context.drawImage(video, 0, 0, canvas.width, canvas.height);
@@ -48,23 +48,18 @@ function savePicture() {
   // Convert the data URL to a base64-encoded string
   var dataURL = picturePreview.src;
 
-  // Perform face detection on the image (before sending it to the server)
-  fetch("/compare_picture", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    // Send the image data as base64-encoded string
-    body: JSON.stringify({ picture: dataURL }),
-  })
-    .then(function (response) {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error("Failed to compare the picture.");
-      }
-    })
-    .then(function (data) {
+  try {
+    const response = await fetch("/compare_picture", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // Send the image data as base64-encoded string
+      body: JSON.stringify({ picture: dataURL }),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
       if (data.match) {
         // Face detected, proceed with saving the picture
         var pictureName = document.getElementById("picture_name").value.trim();
@@ -74,41 +69,37 @@ function savePicture() {
           return;
         }
 
-        fetch("/save_picture", {
+        const saveResponse = await fetch("/save_picture", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
           // Send the image data as base64-encoded string
           body: JSON.stringify({ picture: dataURL, name: pictureName }),
-        })
-          .then(function (response) {
-            if (response.ok) {
-              alert("Picture saved successfully!");
-              closePopup();
-            } else {
-              return response.json().then(function (data) {
-                if (data.error) {
-                  alert(data.error); // Handle specific error response
-                } else {
-                  alert("Failed to save the picture.");
-                }
-              });
-            }
-          })
-          .catch(function (error) {
-            alert("An error occurred while saving the picture.");
-            console.error("Error:", error);
-          });
+        });
+
+        if (saveResponse.ok) {
+          alert("Picture saved successfully!");
+          closePopup();
+        } else {
+          const saveData = await saveResponse.json();
+          if (saveData.error) {
+            alert(saveData.error); // Handle specific error response
+          } else {
+            alert("Failed to save the picture.");
+          }
+        }
       } else {
         // No face detected
         alert("No face detected. Please try again.");
       }
-    })
-    .catch(function (error) {
-      alert("An error occurred while comparing the picture.");
-      console.error("Error:", error);
-    });
+    } else {
+      throw new Error("Failed to compare the picture.");
+    }
+  } catch (error) {
+    alert("An error occurred while comparing or saving the picture.");
+    console.error("Error:", error);
+  }
 }
 
 function tryAgain() {
