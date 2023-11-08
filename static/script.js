32b1781337a5bca9ca124f2e00d70d9ed766c181
nunojeronimos <nuno,jeroimo@jeronimos.com.pt>
2023-11-08 23:40:22 +0000

    new page after login

--- static/script.js
+++ static/script.js
@@ -169,7 +169,7 @@ function Login() {
     })
     .then(function (data) {
       if (data.match) {
-        //alert("Welcome, " + data.name + "!");
+        alert("ehehehe, " + data.name + "!");
         window.location.href = `/profile?name=${data.name}`;
       } else {
         alert("No match found.");
