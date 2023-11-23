b7327d1dab2e925b88c657f34441a75a3a3abb19
nunojeronimos <nuno,jeroimo@jeronimos.com.pt>
2023-11-23 02:22:32 +0000

    fixing improving using aug_data

--- static/script.js
+++ static/script.js
@@ -181,7 +181,7 @@ function Login() {
     })
     .then(function (data) {
       if (data.match) {
-        alert("ehehe, " + data.name + "!");
+        alert("champions, " + data.name + "!");
         window.location.href = `/profile?name=${data.name}`;
       } else {
         alert("No match found.");
