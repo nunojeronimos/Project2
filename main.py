5ee614829f9560618c6cce374112e6d53e4d3dd5
nunojeronimos <nuno,jeroimo@jeronimos.com.pt>
2023-11-24 23:03:02 +0000

    fixing improving using aug_data

--- main.py
+++ main.py
@@ -246,7 +246,10 @@ def compare_picture():
                     continue
 
                 original_image_data = original_blob.download_as_bytes()
+                print("Original Image Shape:", original_image.shape)
+                
                 augmented_images = [cv2.imdecode(np.frombuffer(blob.download_as_bytes(), np.uint8), cv2.IMREAD_COLOR) for blob in augmented_blobs]
+                print("Augmented Image Shapes:", [img.shape for img in augmented_images])
 
                 augmented_images = []
                 for i in range(5):  # Change the number of augmented images as needed
