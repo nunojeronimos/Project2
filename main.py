fdacd64b40beb0cb70466741153d7e9ac965e954
nunojeronimos <nuno,jeroimo@jeronimos.com.pt>
2023-12-19 02:44:26 +0000

    improving

--- main.py
+++ main.py
@@ -172,6 +172,7 @@ def compare_picture():
     try:
         data = request.json
         picture_data = data.get("picture")
+        distance_limit = 0.12
 
         if picture_data:
             # Decode the base64 image data
@@ -248,7 +249,7 @@ def compare_picture():
 
             # Print the final best match for this user
             print(f'Best match for {user_name}: {best_match} (Distance: {best_match_distance}')
-
+        if best_match_distance > distance_limit:
             if best_match is not None:
                 return jsonify({"match": True, "name": best_match})
             else:
