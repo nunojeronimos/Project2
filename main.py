5f176a59d6b75ee4edca4d0f4b3b5fb762bf6810
nunojeronimos <nuno,jeroimo@jeronimos.com.pt>
2023-12-19 02:07:45 +0000

    improving

--- main.py
+++ main.py
@@ -172,6 +172,7 @@ def compare_picture():
     try:
         data = request.json
         picture_data = data.get("picture")
+        threshold = 0.12
 
         if picture_data:
             # Decode the base64 image data
@@ -249,7 +250,7 @@ def compare_picture():
             # Print the final best match for this user
             print(f'Best match for {user_name}: {best_match} (Distance: {best_match_distance}')
 
-            if best_match is not None:
+            if best_match is not None and best_match_distance <= threshold:
                 return jsonify({"match": True, "name": best_match})
             else:
                 return jsonify({"match": False, "error": "No face detected or no matching user."})
